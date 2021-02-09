f1fd70ac81760fc5db6847520b82a16189507d8d
hayrapetyanstyle <hayrapetyanstyle@gmail.com>
2021-02-09 23:24:26 +0400

    project day 6

--- project/js/script.js
+++ project/js/script.js
@@ -2,125 +2,183 @@ window.addEventListener('load', () => {
     const mobileMenuBtn = document.querySelector('#mobile_menu_btn');
     const navMenu = document.querySelector('#nav_menu');
 
-    const offerModalBtn = document.querySelectorAll('[data-modal-btn]');
-    const offerModal = document.querySelector('[data-offer-modal]');
-    const offerModalClose = document.querySelector('[data-offer-close]');
-    const offerPrice = document.querySelector('[data-offerPrice]');
-    const offerModalSubmit = document.querySelector('[data-modal-submit]');
-    const offerModalH3 = document.querySelector('[data-offer-modal] h3');
-
-    const offerItem = document.querySelectorAll('.offer_item h3');
-
-    const countModalBtn = document.querySelector('.member_count');
-    const countModal = document.querySelector('[data-memberCount-modal]');
-    const countModalClose = document.querySelector('[data-count-close]')
-
-    const minusBtn = document.querySelectorAll('[data-minus]');
-    const plusBtn = document.querySelectorAll('[data-plus]');
-    const counterResault = document.querySelectorAll('[data-resault]');
+    const silver = `57.844 AMD`;
+    const gold = `98.455 AMD`;
+    const vip = `250.485 AMD`;
 
     mobileMenuBtn.addEventListener('click', () => {
         navMenu.classList.toggle('menu_active');
     });
 
-    offerModalBtn.forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-            offerModal.style.display = 'flex';
-            let silver = `57.844 AMD`;
-            let gold = `98.455 AMD`;
-            let vip = `250.485 AMD`;
+    try {
+        const modalPreview = (btn, modal, close) => {
+            btn = document.querySelector(btn);
+            close = document.querySelector(close);
+            modal = document.querySelector(modal);
 
-            if (i == 0) {
-                offerModalH3.innerHTML = `Վճարել։ ${offerItem[0].innerHTML}ի համար`;
-                offerPrice.innerHTML = `Վճարման ենթակա գումարը։ <span> ${silver}</span>`;
-                offerModalSubmit.setAttribute('value', `Վճարել ${silver}`)
-            }
-            if (i == 1) {
-                offerModalH3.innerHTML = `Վճարել։ ${offerItem[1].innerHTML}ի համար`;
-                offerPrice.innerHTML = `Վճարման ենթակա գումարը։ <span> ${gold}</span>`;
-                offerModalSubmit.setAttribute('value', `Վճարել ${gold}`)
-            }
-            if (i == 2) {
-                offerModalH3.innerHTML = `Վճարել։ ${offerItem[2].innerHTML}ի համար`;
-                offerPrice.innerHTML = `Վճարման ենթակա գումարը։ <span> ${vip}</span>`;
-                offerModalSubmit.setAttribute('value', `Վճարել ${vip}`)
-            }
-        });
-    });
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                modal.style.display = 'flex';
+            });
 
-    offerModalClose.addEventListener('click', () => {
-        offerModal.style.display = 'none';
-    });
+            close.addEventListener('click', () => {
+                modal.style.display = 'none';
+            });
+        };
 
-    countModalBtn.addEventListener('click', () => {
-        countModal.style.display = 'flex';
-    });
+        modalPreview('#logBtn', '[data-loginOrReg-modal]', '[data-close-logmodal]');
+        modalPreview('.member_count', '[data-memberCount-modal]', '[data-count-close]');
+    } catch {
+        console.log('error preview modal');
+    }
 
-    countModalClose.addEventListener('click', () => {
-        countModal.style.display = 'none';
-    });
+    try {
+        const offerModalContent = (price, submit, title, money, offerItemh3) => {
+            title.innerHTML = `Վճարել։ ${offerItemh3.innerHTML}ի համար`;
+            price.innerHTML = `Վճարման ենթակա գումարը։ <span> ${money}</span>`;
+            submit.setAttribute('value', `Վճարել ${money}`);
+        };
 
-    const counterFilter = (minus, plus, resault) => {
-        let i = 0;
+        const offerModalPreview = (button, modal, close, price, submit, title, offerItem) => {
+            button = document.querySelectorAll(button);
+            modal = document.querySelector(modal);
+            close = document.querySelector(close);
+            price = document.querySelector(price);
+            submit = document.querySelector(submit);
+            title = document.querySelector(title);
+            offerItem = document.querySelectorAll(offerItem);
 
-        minus.addEventListener('click', () => {
-            if (resault.innerHTML >= 1) {
-                resault.innerHTML = --i;
-            }
-        });
+            button.forEach((btn, i) => {
+                btn.addEventListener('click', () => {
+                    modal.style.display = 'flex';
 
-        plus.addEventListener('click', () => {
-            if (resault.innerHTML <= 8) {
-                resault.innerHTML = ++i;
-            }
-        });
-    };
+                    if (i == 0) {
+                        offerModalContent(price, submit, title, silver, offerItem[0]);
+                    }
+                    if (i == 1) {
+                        offerModalContent(price, submit, title, gold, offerItem[1]);
+                    }
+                    if (i == 2) {
+                        offerModalContent(price, submit, title, vip, offerItem[2]);
+                    }
+                });
+            });
 
-    counterFilter(minusBtn[0], plusBtn[0], counterResault[0]);
-    counterFilter(minusBtn[1], plusBtn[1], counterResault[1]);
-    counterFilter(minusBtn[2], plusBtn[2], counterResault[2]);
-
-    const swiper = new Swiper('.swiper-container', {
-        slidesPerView: 5,
-        spaceBetween: 15,
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'progressbar',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            520: {
-                slidesPerView: 1,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            980: {
-                slidesPerView: 3,
-            },
-            1140: {
-                slidesPerView: 4,
-            }
+            close.addEventListener('click', () => {
+                modal.style.display = 'none';
+            });
         }
-    });
 
-    const swiperSlide = document.querySelectorAll('.swiper-slide');
+        offerModalPreview('[data-modal-btn]', '[data-offer-modal]', '[data-offer-close]', '[data-offerPrice]', '[data-modal-submit]', '[data-offer-modal] h3', '.offer_item h3');
 
-    swiperSlide.forEach(slide => {
-        slide.addEventListener('mouseover', () => {
-            swiper.autoplay.stop();
+    } catch {
+        console.log('error offer modal');
+    }
+
+    try {
+        const minusBtn = document.querySelectorAll('[data-minus]');
+        const plusBtn = document.querySelectorAll('[data-plus]');
+        const counterResault = document.querySelectorAll('[data-resault]');
+
+        const counterFilter = (minus, plus, resault) => {
+            let i = 0;
+    
+            minus.addEventListener('click', (e) => {
+                e.preventDefault()
+                if (resault.innerHTML >= 1) {
+                    resault.innerHTML = --i;
+                }
+            });
+    
+            plus.addEventListener('click', (e) => {
+                e.preventDefault()
+                if (resault.innerHTML <= 8) {
+                    resault.innerHTML = ++i;
+                }
+            });
+        };
+
+        counterFilter(minusBtn[0], plusBtn[0], counterResault[0]);
+        counterFilter(minusBtn[1], plusBtn[1], counterResault[1]);
+        counterFilter(minusBtn[2], plusBtn[2], counterResault[2]);
+
+    } catch {
+        console.log('error counter');
+    }
+
+    try {
+        const swiper = new Swiper('.swiper-container', {
+            slidesPerView: 5,
+            spaceBetween: 15,
+            loop: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'progressbar',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                520: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                980: {
+                    slidesPerView: 3,
+                },
+                1140: {
+                    slidesPerView: 4,
+                }
+            }
         });
-        
-        slide.addEventListener('mouseleave', () => {
-            swiper.autoplay.start();
+    
+        const swiperSlide = document.querySelectorAll('.swiper-slide');
+    
+        swiperSlide.forEach(slide => {
+            slide.addEventListener('mouseover', () => {
+                swiper.autoplay.stop();
+            });
+            
+            slide.addEventListener('mouseleave', () => {
+                swiper.autoplay.start();
+            });
         });
-    });
+    } catch {
+        console.error('error in swiper');
+    }
+
+    try {
+        const logTab = document.querySelector('[data-log]');
+        const logBtn = document.querySelector('.open_log');
+
+        const regTab = document.querySelector('[data-reg]');
+        const regBtn = document.querySelector('.open_reg');
+
+        const tabConfig = (activeBtn, deactiveBtn, currentClass, showTab, hideTab) => {
+            activeBtn.addEventListener('click', () => {
+                deactiveBtn.classList.remove(currentClass);
+                activeBtn.classList.add(currentClass);
+                hideTab.style.display = 'none';
+                showTab.style.display = 'block';
+            });
+        }
+
+        logBtn.addEventListener('click', () => {
+            tabConfig(logBtn, regBtn, 'active_tab', logTab, regTab);
+        });
+
+        regBtn.addEventListener('click', () => {
+            tabConfig(regBtn, logBtn, 'active_tab', regTab, logTab);
+        });
+
+    } catch {
+        console.log('error in tabs');
+    }
 });
